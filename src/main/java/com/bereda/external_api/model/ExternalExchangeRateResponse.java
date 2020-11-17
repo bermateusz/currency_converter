d515f3cc1df7da482523f7e21129fcf723865045
bermateusz <beredamateusz@gmail.com>
2020-11-17 17:16:02 +0100

    lombok annotation added

--- src/main/java/com/bereda/external_api/model/ExternalExchangeRateResponse.java
+++ src/main/java/com/bereda/external_api/model/ExternalExchangeRateResponse.java
@@ -19,22 +19,5 @@ public class ExternalExchangeRateResponse {
         this.base = base;
         this.rates = rates;
     }
-
-//    public String getBase() {
-//        return base;
-//    }
-//
-//    public void setBase(String base) {
-//        this.base = base;
-//    }
-//
-//    public Map<String, Double> getRates() {
-//        return rates;
-//    }
-//
-//    public void setRates(Map<String, Double> rates) {
-//        this.rates = rates;
-//    }
-
 }
 
